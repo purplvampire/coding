42341ec533721cc77cb60a378772cb66a602ebc5
Jason Chen <purplvampire@gmail.com>
2019-04-02 21:08:45 +0800

    add dict.py

---
+++ dict.py
@@ -0,0 +1,13 @@
+# dictionary 字典
+# 單行式寫法
+words = {'ramen': '拉麵', 'pasta': '義大利麵'} 
+# 多行式寫法(正規寫法)
+words = {
+    'ramen': '拉麵',
+    'pasta': '義大利麵'
+}
+# 增加新的key
+words['tea'] = '茶'
+# 查字典中的答案
+print(words['tea'])
+print(words)
